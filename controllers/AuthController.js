fec5cc01eb92ea3051dba05c1f2b9f0cc849654d
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2024-02-19 23:52:53 +0530

    password_encryption

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,13 +1,20 @@
 import prisma from "../DB/db.config.js";
 import vine, { errors } from "@vinejs/vine";
 import { registerSchema } from "../validations/authValidation.js";
+import bcrypt from 'bcryptjs'
 
 class AuthController {
   static async register(req, res) {
     try {
       const body = req.body;
-      const validator = vim.compile(registerSchema);
+      const validator = vine.compile(registerSchema);
+
       const payload = await validator.validate(body);
+
+      // Password Encryption
+      
+      const salt = bcrypt.genSaltSync(10)
+      payload.password = bcrypt.hashSync(payload.password, salt)
 
       return res.json({
         payload,
@@ -21,3 +28,5 @@ class AuthController {
     }
   }
 }
+
+export default AuthController;
--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-import express from "express"
-import "dotenv/config"
+import express from "express";
+import "dotenv/config";
+import ApiRoutes from './routes/api.js'
 
-const app = express()
-const PORT = process.env.PORT || 8000
+const app = express();
+const PORT = process.env.PORT || 8000;
 
-app.get('/', (req, res) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
   return res.json({
-    message: "hello from server"
-  })
-})
+    message: "hello from server",
+  });
+});
+
+app.use("/api", ApiRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on PORT ${PORT}`)
-})
+  console.log(`Server is running on PORT ${PORT}`);
+});
