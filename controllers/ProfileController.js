dd5f2540d6065b3dadde843f953aff7f5674562e
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2024-02-20 14:41:48 +0530

    feat:file_upload

--- controllers/ProfileController.js
+++ controllers/ProfileController.js
@@ -1,3 +1,6 @@
+import prisma from "../DB/db.config.js";
+import { generateUniqueId, imageValidator } from "../utils/helper.js";
+
 class ProfileController {
   static async index(req, res) {
     try {
@@ -14,16 +17,48 @@ class ProfileController {
   }
   static async store() {}
   static async show() {}
-  static async update() {
+  static async update(req, res) {
     try {
       const { id } = req.params;
-      const authUser = req.user;
       if (!req.files || Object.keys(req.files).length === 0) {
         return res.status(400).json({
           status: 400,
           message: "Image is required ",
         });
       }
+
+      const profile = req.files.profile;
+      const message = imageValidator(profile?.size, profile?.mimetype);
+
+      if (message !== null) {
+        return res.status(400).json({
+          error: {
+            profile: message,
+          },
+        });
+      }
+
+      const imgExt = profile?.name.split(".");
+      const imageName = `${generateUniqueId()}.${imgExt[1]}`;
+      const uploadPath = `${process.cwd()}/public/images/${imageName}`;
+
+      profile.mv(uploadPath, (err) => {
+        if (err) throw err;
+      });
+
+      await prisma.users.update({
+        data: {
+          profile: imageName,
+        },
+        where: {
+          id: Number(id),
+        },
+      });
+
+      return res.json({
+        status: 200,
+        message: "successfully updated profile",
+      });
     } catch (error) {
       return res.status(500).json({
         message: "Something went wrong!",
--- utils/helper.js
+++ utils/helper.js
@@ -1,9 +1,20 @@
-export const imageValidator = (size, mime) =>{
- if(bytesToMb(size) > 2){
-    return "Image size must be less than 2 MB"
-  }
-}
+import { supportedMimes } from "../config/filesystem.js";
+import { v4 as uuid } from "uuid";
 
-export bytesToMb = (bytes) =>{
-  return bytes / (1024 * 1024)
-}
+export const imageValidator = (size, mime) => {
+  if (!supportedMimes.includes(mime)) {
+    return "Image must be png,jpg,jpeg,svg,gif,webp";
+  } else if (bytesToMb(size) > 2) {
+    return "Image size must be less than 2 MB";
+  }
+
+  return null;
+};
+
+export const bytesToMb = (bytes) => {
+  return bytes / (1024 * 1024);
+};
+
+export const generateUniqueId = () => {
+  return uuid();
+};
