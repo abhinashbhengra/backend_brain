affa74cbb3542bccdf35409d89ecd65902bd1957
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2024-02-20 11:38:49 +0530

    Auth_Route_Update

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import "dotenv/config";
-import ApiRoutes from './routes/api.js'
+import ApiRoutes from "./routes/api.js";
+import fileUpload from "express-fileupload";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(fileUpload());
 
 app.get("/", (req, res) => {
   return res.json({
