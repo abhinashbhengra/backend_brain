de448bca64b3fbe289a20ab6efbeee14a047e75d
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2024-02-20 09:45:08 +0530

    check_for_existing_email

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,7 +1,7 @@
 import prisma from "../DB/db.config.js";
 import vine, { errors } from "@vinejs/vine";
 import { registerSchema } from "../validations/authValidation.js";
-import bcrypt from 'bcryptjs'
+import bcrypt from "bcryptjs";
 
 class AuthController {
   static async register(req, res) {
@@ -11,18 +11,44 @@ class AuthController {
 
       const payload = await validator.validate(body);
 
+      // Check for existing email
+      const findUser = await prisma.users.findUnique({
+        where: {
+          email: payload.email,
+        },
+      });
+
+      if (findUser) {
+        return res.status(400).json({
+          error: {
+            email: "Email already taken.",
+          },
+        });
+      }
+
       // Password Encryption
-      
-      const salt = bcrypt.genSaltSync(10)
-      payload.password = bcrypt.hashSync(payload.password, salt)
+
+      const salt = bcrypt.genSaltSync(10);
+      payload.password = bcrypt.hashSync(payload.password, salt);
+
+      const user = await prisma.users.create({
+        data: payload,
+      });
 
       return res.json({
-        payload,
+        status: 200,
+        message: "successfully created user",
+        user,
       });
     } catch (error) {
       if (error instanceof errors.E_VALIDATION_ERROR) {
         return res.status(400).json({
           errors: error.message,
+        });
+      } else {
+        return res.status(500).json({
+          status: 500,
+          message: "Something went wrong!!",
         });
       }
     }
